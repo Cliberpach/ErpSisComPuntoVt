e68b35dc25d3bcf777371dea153de6238d7077b8
pablolomusan@hotmail.com <pablolomusan@hotmail.com>
2021-12-02 22:39:49 +0000

    cambios desde el servidor

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -37,8 +37,22 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: 'ASDASF2121',
     wsHost: window.location.hostname,
-    wsPort: 6001,
+    wssPort: 6001,
+    encrypted:false,
+    disableStats:true,
     enabledTransports: ['ws', 'wss'],
+    //forceTLS:false,
+});
+/* En el local
+
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: 'ASDASF2121',
+    wsHost: window.location.hostname,
+    wsPort: 6001,
     forceTLS:false,
     disableStats: true,
 });
+
+*/
+
