67e419fd0fa3721ec2276b61c314504ac89a6d9f
CarlosCubas16 <ccubas@unitru.edu.pe>
2022-03-08 17:14:22 -0500

    update

--- resources/views/home_aux.blade.php
+++ resources/views/home_aux.blade.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-12">
             <div class="ibox " style="height: 100vh;">
-                <img src="/img/banner.jpg" alt="SisCom" style="width: 100%; height: 100%">
+                <img src="/img/erp_logo_long.jpeg" alt="SisCom" style="width: 100%; height: 100%">
             </div>
         </div>
     </div>
